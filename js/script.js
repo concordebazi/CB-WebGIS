339a36366b0a40a5127357c4db3bca9670685e28
concorde bazimaziki <59648271+concordebazi@users.noreply.github.com>
2026-09-01 10:36:04 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,13 +16,10 @@ if (menuToggle && navLinks) {
         function () {
 
             menuToggle.classList.toggle("active");
-
             navLinks.classList.toggle("active");
-
 
             const isOpen =
             navLinks.classList.contains("active");
-
 
             menuToggle.setAttribute(
                 "aria-expanded",
@@ -46,62 +43,6 @@ if (menuToggle && navLinks) {
 
                     navLinks.classList.remove("active");
 
-                    menuToggle.classList.remove("active");
-
-                    menuToggle.setAttribute(
-                        "aria-expanded",
-                        "false"
-                    );
-
-                }
-            );
-
-        }
-    );
-
-}
-// ==========================================
-// MOBILE NAVIGATION
-// ==========================================
-
-const menuToggle =
-document.getElementById("menu-toggle");
-
-const navLinks =
-document.querySelector(".nav-links");
-
-if (menuToggle && navLinks) {
-
-    menuToggle.addEventListener(
-        "click",
-        function () {
-
-            menuToggle.classList.toggle("active");
-            navLinks.classList.toggle("active");
-
-            const isOpen =
-            navLinks.classList.contains("active");
-
-            menuToggle.setAttribute(
-                "aria-expanded",
-                isOpen
-            );
-
-        }
-    );
-
-
-    const navItems =
-    navLinks.querySelectorAll("a");
-
-    navItems.forEach(
-        function (link) {
-
-            link.addEventListener(
-                "click",
-                function () {
-
-                    navLinks.classList.remove("active");
                     menuToggle.classList.remove("active");
 
                     menuToggle.setAttribute(
